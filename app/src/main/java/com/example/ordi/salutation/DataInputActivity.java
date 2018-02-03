92e3a861138e6f052b330150d1a1d4e8596c0976
Misha1902 <misha.ib19@gmail.com>
2018-02-03 17:58:42 +0300

    NavigationActivity_2.1 (Statusbar fixed)

--- app/src/main/java/com/example/ordi/salutation/DataInputActivity.java
+++ app/src/main/java/com/example/ordi/salutation/DataInputActivity.java
@@ -1,13 +1,13 @@
 package com.example.ordi.salutation;
 
-        import android.content.res.ColorStateList;
-        import android.graphics.Color;
-        import android.os.Bundle;
-        import android.support.design.widget.FloatingActionButton;
-        import android.support.design.widget.Snackbar;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.View;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class DataInputActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/ordi/salutation/NavigationActivity.java
+++ app/src/main/java/com/example/ordi/salutation/NavigationActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
