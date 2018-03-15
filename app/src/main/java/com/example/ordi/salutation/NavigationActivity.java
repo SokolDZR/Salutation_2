7fa0faba2c0122e1dc0a04ab419bccff54a2d447
Misha1902 <misha.ib19@gmail.com>
2018-03-15 21:05:17 +0300

    NavigationActivity_3.4 (SettingsFragment added, fab button corrected, settings button removed from MainActivity, removed top menu, fragment title added)

--- app/src/main/java/com/example/ordi/salutation/NavigationActivity.java
+++ app/src/main/java/com/example/ordi/salutation/NavigationActivity.java
@@ -5,8 +5,7 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,9 +15,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    DataInputFragment fDataInput;
+    SelectSalutationFragment fSelectSalutation;
+    InstructionFragment fInstruction;
+    AboutUsFragment fAboutUs;
+    SettingsFragment fSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +34,14 @@ public class NavigationActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton navigationFab = (FloatingActionButton) findViewById(R.id.navigationFab);
-        navigationFab.setBackgroundTintList(ColorStateList.valueOf(Color
-                .parseColor("#13c97a")));
-
+        final FloatingActionButton navigationFab = (FloatingActionButton) findViewById(R.id.NavigationFab);
         navigationFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), SelectSalutation.class));
+                FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
+                fTrans.replace(R.id.container, fSelectSalutation);
+                fTrans.commit();
+                navigationFab.hide();
             }
         });
 
@@ -45,6 +53,11 @@ public class NavigationActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        fDataInput = new DataInputFragment();
+        fSelectSalutation = new SelectSalutationFragment();
+        fInstruction = new InstructionFragment();
+        fAboutUs = new AboutUsFragment();
+        fSettings = new SettingsFragment();
     }
 
     @Override
@@ -57,45 +70,39 @@ public class NavigationActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.navigation, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this, Settings.class);
-            startActivity(settingsIntent);
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        final FloatingActionButton navigationFab = (FloatingActionButton) findViewById(R.id.NavigationFab);
         int id = item.getItemId();
-
-        if (id == R.id.seleclSalutation) {
-            Intent seleclSalutation = new Intent(this, SelectSalutation.class);
-            startActivity(seleclSalutation);
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
+        fTrans.addToBackStack(null);
+        if (id == R.id.selectSalutation) {
+            fTrans.replace(R.id.container, fSelectSalutation);
+            navigationFab.hide();
+            setTitle(getString(R.string.title_select_salutation));
+        } else if (id == R.id.settings_item) {
+            fTrans.replace(R.id.container, fSettings);
+            setTitle(getString(R.string.title_select_salutation));
+            navigationFab.show();
+        } else if (id == R.id.aboutUs_item) {
+            fTrans.replace(R.id.container, fAboutUs);
+            setTitle(getString(R.string.title_about_us));
+            navigationFab.show();
+        } else if (id == R.id.instruction_item) {
+            fTrans.replace(R.id.container, fInstruction);
+            setTitle(getString(R.string.title_instruction));
+            navigationFab.show();
         }
-
+        fTrans.commit();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
